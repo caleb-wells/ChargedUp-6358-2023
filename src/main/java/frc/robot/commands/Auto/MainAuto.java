5788707b2067018199ec9fedf5015a5dd5787c64
Caleb Wells <wcaleb650@gmail.com>
2023-02-23 10:30:42 -0700

    Refactor code, updates almost all files

---
+++ src/main/java/frc/robot/commands/Auto/MainAuto.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Auto;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+//import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.DriveForward;
+
+public class MainAuto extends SequentialCommandGroup {
+
+  public MainAuto() {
+    addCommands(
+      new DriveForward(1),
+      //new WaitCommand(4),
+      new DriveForward(-1));
+  }
+  
+}
