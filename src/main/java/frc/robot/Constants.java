c7613e5adaae1b225d03a2b56078dbf90e9098cd
Caleb Wells <wcaleb650@gmail.com>
2023-03-06 22:56:19 -0700

    Update constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -107,7 +107,7 @@ public final class Constants {
     public static final double kTurningEncoderPositionPIDMinInput = 0; // radians
     public static final double kTurningEncoderPositionPIDMaxInput = kTurningEncoderPositionFactor; // radians
 
-    public static final double kDrivingP = 0.007;
+    public static final double kDrivingP = 0.009;
     public static final double kDrivingI = 0.001;
     public static final double kDrivingD = 0;
     public static final double kDrivingFF = 1 / kDriveWheelFreeSpeedRps;
